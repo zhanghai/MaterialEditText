d5a76c2b7ba4bda630fdda8da59645ddb68e67b0
Zhang Hai <dreaming.in.code.zh@gmail.com>
2015-10-14 19:52:52 +0800

    Finish sample.

---
+++ sample/src/main/java/me/zhanghai/android/materialedittext/sample/AboutActivity.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
+ * All Rights Reserved.
+ */
+
+package me.zhanghai.android.materialedittext.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Bind(R.id.version)
+    TextView mVersionText;
+    @Bind(R.id.github)
+    TextView mGithubText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.about_activity);
+        ButterKnife.bind(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String version = getString(R.string.about_version_format,
+                AppUtils.getPackageInfo(this).versionName);
+        mVersionText.setText(version);
+        mGithubText.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                AppUtils.navigateUp(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- sample/src/main/java/me/zhanghai/android/materialedittext/sample/MainActivity.java
+++ sample/src/main/java/me/zhanghai/android/materialedittext/sample/MainActivity.java
@@ -5,15 +5,23 @@
 
 package me.zhanghai.android.materialedittext.sample;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.error_native_edit)
+    EditText mErrorNativeEdit;
+    @Bind(R.id.error_material_edit)
+    EditText mErrorMaterialEdit;
     @Bind(R.id.error_edit_layout)
     TextInputLayout mErrorEditLayout;
 
@@ -24,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
         ButterKnife.bind(this);
 
+        mErrorNativeEdit.setError("An error occurred");
+        mErrorMaterialEdit.setError("An error occurred");
         mErrorEditLayout.setError("An error occurred");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                startActivity(new Intent(this, AboutActivity.class));
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
